a2966c9e18224a261720e5c481d125652f7795c4
Jake <jacobstefan92@gmail.com>
2018-10-14 23:44:19 +0100

    Updated regex to support sentences and title case, made basic UI

--- scripts.js
+++ scripts.js
@@ -1,16 +1,45 @@
-function translatePigLatin(str) {
-	const reFindVowel = /\b[aeiouAEIOU]/g;
-	const reFindConsonant = /\b[^aeiouAEIOU]+/g;
-	return reFindVowel.test(str) ? append(str) : remove(str);
+let clear = document.querySelector('.clear');
 
-	function append(str) {
-		return str.concat("way");
-	}
-
-	function remove(str) {
-		let start = str.match(reFindConsonant);
-		return str.replace(reFindConsonant, "").concat(start + "ay");
-	}
+function clearOutput() {
+	document.getElementById("answer").innerHTML = "";
 }
 
-translatePigLatin("eight");
+clear.addEventListener('click', clearOutput);
+
+
+validate = function() {                                                     
+	let name = document.getElementById("english-input").value;              // taking input & assign to variable 
+	let translation = translatePigLatin(name);                              // assign trans func to variable
+
+	document.getElementById("answer").innerHTML = translation;              // call function and assign result to id
+	document.getElementById("pigForm").reset();
+
+	
+}
+
+// translator function
+function translatePigLatin(str) {
+	let arr = str.split(" ").map(el => {
+
+		const reFindVowel = /\b[aeiouAEIOU]/g;
+	const reFindConsonant = /\b[^aeiouAEIOU]+/g;
+	return reFindVowel.test(el) ? append(el) : remove(el);
+
+	function append(el) {
+		return el.concat("way").toLowerCase();
+	}
+
+	function remove(el) {
+		let start = el.match(reFindConsonant);
+		return el.replace(reFindConsonant, "").concat(start + "ay").toLowerCase();
+	}
+
+
+	});
+
+	
+
+	let newStr = arr.join(" ");
+	let cap = newStr.charAt(0).toUpperCase();
+	return `${newStr.replace(/^[a-z]/ig, cap)}`
+}  
