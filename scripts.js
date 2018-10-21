f9c141de9842e5a323e1f872ffc9b3d1f2b1bffa
Jake <jacobstefan92@gmail.com>
2018-10-21 15:38:56 +0100

    added speech recognition

--- scripts.js
+++ scripts.js
@@ -1,26 +1,60 @@
 let clear = document.querySelector('.clear');
+const myForm = document.forms['pigForm'];
+const output = document.querySelector('#answer');
 
+// Submit function
+function submit(e) {
+	e.preventDefault();
+	let name = myForm.querySelector('input[type="text"]').value;
+	if (name == "") return false;	
+	const translation = translatePigLatin(name);
+	output.textContent = translation;	
+	document.forms["pigForm"].reset();
+	window.scrollTo(0,0);
+};
+
+myForm.addEventListener('submit', submit);
+
+//Clear function
 function clearOutput() {
 	document.getElementById("answer").innerHTML = "";
 }
 
 clear.addEventListener('click', clearOutput);
 
-	const myForm = document.forms['pigForm'];
-	const output = document.querySelector('#answer');
+	
+// Text to speech
 
-	myForm.addEventListener('submit', function(e) {
-		e.preventDefault();
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-		const name = myForm.querySelector('input[type="text"]').value;
-		if (name == "") return false;
-		
-		const translation = translatePigLatin(name);
-		output.textContent = translation;
-		
-		document.forms["pigForm"].reset();
-		window.scrollTo(0,0);
-	});
+    const recognition = new SpeechRecognition();
+	recognition.interimResults = true;
+	
+	// let p = document.createElement('p');
+	// p.classList.add('speech');
+    // const words = document.querySelector('.input');
+	// words.append(p);
+	
+	recognition.addEventListener('result', e => {
+        const transcript = Array.from(e.results)
+            .map(result => result[0])
+            .map(result => result.transcript)
+            .join('')
+            // p.textContent = transcript;
+            if(e.results[0].isFinal) {
+                myForm.querySelector('input[type="text"]').value = transcript;
+            }
+			console.log(transcript);
+    });
+	recognition.addEventListener('end', recognition.start);
+	recognition.addEventListener('end', submit);
+	
+    
+	recognition.start();
+	
+
+
+	
 
 // translator function
 function translatePigLatin(str) {
