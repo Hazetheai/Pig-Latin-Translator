01577a152c5326bf1050aae0b3713fe3a72d0522
Jake <jacobstefan92@gmail.com>
2018-10-26 14:15:19 +0100

    added speech synthesis

--- scripts.js
+++ scripts.js
@@ -9,8 +9,10 @@ function submit(e) {
 	if (name == "") return false;	
 	const translation = translatePigLatin(name);
 	output.textContent = translation;	
-	document.forms["pigForm"].reset();
+	// document.forms["pigForm"].reset();
 	window.scrollTo(0,0);
+	getMsg();
+	speak();
 };
 
 myForm.addEventListener('submit', submit);
@@ -22,8 +24,14 @@ function clearOutput() {
 
 clear.addEventListener('click', clearOutput);
 
+//Speak function
+
+function speak() {
+	toggle();
+}
+
 	
-// Text to speech
+// Speech to text
 
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
@@ -51,6 +59,65 @@ window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogn
 	
     
 	recognition.start();
+
+
+	// Text to Speech
+
+	 //set variables
+	 const msg = new SpeechSynthesisUtterance();
+	 let voices = [];
+	 const voicesDropdown = document.querySelector('[name="voice"]');
+	 const options = document.querySelectorAll('[type="range"], [name="text"]');
+	 const speakButton = document.querySelector('#speak');
+	 const stopButton = document.querySelector('#stop');
+   
+   
+	 //the text we are speaking
+	 function getMsg() {
+		msg.text = document.querySelector('#answer').textContent;
+	 	console.log(msg.text);
+	 }
+
+	 
+   
+   
+	 //put voices from Chromes APi into list
+	 function populateVoices() {
+		 voices = this.getVoices();
+		 voicesDropdown.innerHTML  = voices
+		 .filter(voice => voice.lang.includes('en'))
+		   .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+		   .join('');
+	 }
+   
+   
+	 //set the voice
+	 function setVoice() {
+		 msg.voice = voices.find(voice => voice.name === this.value);
+		 console.log(msg);
+   
+	}
+	 //restart phrase if interrupted
+	  function toggle(startOver = true) {
+	   speechSynthesis.cancel();
+	   if (startOver) {
+		 speechSynthesis.speak(msg);
+	   }
+	 }
+	 // choose options
+	 function setOption() {
+		 console.log(this.name, this.value);
+		 msg[this.name] = this.value;
+		 toggle();
+	 }
+   
+	 speechSynthesis.addEventListener('voiceschanged', populateVoices);
+	 voicesDropdown.addEventListener('change', setVoice);
+   
+   options.forEach(option => option.addEventListener('change', setOption));
+   
+   speakButton.addEventListener('click', toggle);
+   stopButton.addEventListener('click', () => toggle(false));
 	
 
 
